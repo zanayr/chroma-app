a5e1fecd2dd957284b661f20e806ab573d25bdec
Ryan Fickenscher <rsficken@gmail.com>
2019-08-19 00:11:54 -0700

    cleaning up

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -28,7 +28,6 @@ class Main extends Component {
         value: '#f8f8f8',
     };
 
-    //  Component Did Update  //
     componentDidMount () {
         const saved = database.get('saved');
         this.fill(saved.length ? saved[0] : '#f8f8f8');
@@ -39,15 +38,10 @@ class Main extends Component {
         }));
     }
 
-    //  METHODS  //
     sanitize (value) {
-        //  Sanitize cleans up a passed string value, removing all semicolons and extra
-        //  white-space.
         return value.replace(/;/g, '').replace(/\s/g, ' ').trim();
     }
     fill (value) {
-        //  Fill saves the current color as the last color and sets a passed color
-        //  value as the new color, updating the state.
         const last = this.state.color || '#f8f8f8';
         const color = this.sanitize(value);
         this.setState(prev => ({
@@ -61,8 +55,6 @@ class Main extends Component {
         }));
     }
     clear () {
-        //  Clear saves the current color as the last color and sets the current color
-        //  to `#f8f8f8`.
         const last = this.state.color;
         this.setState(prev => ({
             ...prev,
@@ -75,20 +67,17 @@ class Main extends Component {
         }));
     }
     remove () {
-        //  Remove deletes a color from the saved list in the state and local storage.
         const color = this.state.color;
         const saved = this.state.saved;
         let index;
         let next;
         let updated;
-        //  Check if the color exists in the saved array
         if (this.state.saved.find((v) => {return v === color})) {
             index = saved.indexOf(color);
             updated = saved.filter((v) => {return v !== color});
             next = updated[index - 1] ? updated[index - 1] : updated[index];
             this.setState({saved: updated});
             database.set('saved', updated);
-            //  Update background color and value
             if (next) {
                 this.fill(next);
                 this.setState({value: next});
@@ -99,7 +88,6 @@ class Main extends Component {
         }
     }
     store (value) {
-        //  Store saves a value into the saved array and local storage.
         const saved = this.state.saved.slice();
         let updated;
         if (!saved.find((v) => {return v === value})) {
@@ -109,25 +97,17 @@ class Main extends Component {
         }
     }
     toggle (bool) {
-        //  Toggle switches the foreground color between black and white depending on
-        //  the bool passed to the method.
         return bool ? this.state.foreground : (this.state.foreground === '#f8f8f8' ? '#242424' : '#f8f8f8');
     }
     update (value) {
-        //  Update updates the value of the field.
         this.setState({value: value});
     }
 
-
-
-    //  EVENT HANDLERS  //
     onAdd = () => {
-        //  Handle the add button being clicked
         if (this.state.color)
             this.store(this.state.color);
     };
     onChange = (value) => {
-        //  Handle the field's value being changed
         this.update(value);
         if (chroma.validate(value)) {
             this.fill(value);
@@ -136,16 +116,13 @@ class Main extends Component {
         }
     };
     onClear = () => {
-        //  Handle the clear button being clicked
         this.clear();
         this.setState({value: ''});
     };
     onRemove = () => {
-        //  Handle the remove button being clicked
         this.remove();
     };
     onTransitionEnd = (event) => {
-        //  Handle the color drop animation ending
         if (event.propertyName === 'left') {
             this.setState(prev => ({
                 ...prev,
@@ -155,14 +132,12 @@ class Main extends Component {
         }
     };
     onRestore = (data) => {
-        //  Handle on a dot being clicked
         if (data !== this.state.color) {
             this.fill(data);
             this.setState({value: data});
         }
     };
     onRandom = () => {
-        //  Handle on the random button being clicked
         const color = chroma([Math.round(Math.random() * 255), Math.round(Math.random() * 255), Math.round(Math.random() * 255)]).to('hex');
         this.fill(color);
         this.setState(prev => ({
@@ -172,32 +147,24 @@ class Main extends Component {
         }));
     };
     onUndo = () => {
-        //  Handle on the undo button being clicked
         const color = this.state.last;
         this.fill(color);
         this.setState({value: color});
     };
     onX11 = (data) => {
-        //  Handle the x11 card being clicked
         this.fill(data);
         this.setState({value: data});
     };
 
-    
-
-    //  RENDER METHOD  //
     render () {
-        //  Set dot position
         const x = Math.round((Math.random() * (window.innerWidth - 200)) + 100);
         const y = Math.round((Math.random() * (window.innerHeight - 200)) + 100);
         let list = null;
-        //  If no color exists, don't render the cards
         if (this.state.color)
             list = (<List
                     foreground={this.state.foreground}
                     onClick={this.onX11}
                     value={this.state.color}/>);
-        // Set the background of the html tag
         document.documentElement.style.backgroundColor = chroma(this.state.background).to('rgb');
         return (
             <Aux>
